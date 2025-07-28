c13f93bdbeb8ab497e4161cda92c0df887cc690b
Niranjan Kumar <niranjankumaraina@gmail.com>
2025-07-28 13:47:27 +0530

    redis is connected

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import { createClient } from "redis";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,6 +9,13 @@ app.get("/health", (req, res) => {
     res.send("Health of user service is good");
 });
 connectDB();
+export const redisClient = createClient({ url: process.env.REDIS_URL });
+redisClient
+    .connect()
+    .then(() => console.log("Redis connected successfully"))
+    .catch((err) => {
+    console.log(err);
+});
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ backend/user/dist/model/User.js
@@ -0,0 +1,6 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+}, { timestamps: true });
+export const User = mongoose.model("User", userSchema);
