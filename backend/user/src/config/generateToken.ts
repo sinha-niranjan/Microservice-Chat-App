ba3f95ccb6f205bccb942a7073691d903dae314a
Niranjan Kumar <niranjankumaraina@gmail.com>
2025-07-29 13:46:29 +0530

    authentication: Add isAuth middleware

--- backend/user/src/config/generateToken.ts
+++ backend/user/src/config/generateToken.ts
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import { IUser } from "../model/User.js";
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
-const generateToken = (user: any) => {
+const generateToken = (user: IUser) => {
   const token = jwt.sign({ user }, JWT_SECRET, {
     expiresIn: "15d",
   });
---
+++ backend/user/src/middleware/isAuth.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { IUser } from "../model/User.js";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export interface AuthenticatedRequest extends Request {
+  user?: IUser | null;
+}
+
+export const isAuth = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401).json({ message: "Please Login - No auth header" });
+      return;
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      res.status(401).json({ message: "Please Login - No token provided" });
+      return;
+    }
+    const decodedValue = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+    if (!decodedValue || !decodedValue.user) {
+      res.status(401).json({ message: "Please Login - Invalid token" });
+      return;
+    }
+    req.user = decodedValue.user as IUser;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Please Login - JWT error" });
+  }
+};
